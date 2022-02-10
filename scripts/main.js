45fee1889e8ab0bde56ca35af5eec7d99304ad10
Lawton Peng <5938002@isd535.org>
2022-02-10 11:33:47 -0600

    Canvas clear screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-// Put your JavaScript here
+
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let speed = 7;
+
+let tileCOunt = 20;
+let tileSize = canvas.width / tileCount 
+let headX = 10;
+let headY = 10;
+
+// game loop
+function drawGame() {
+    clearScreen();
+    setTimeout(drawGame, 1000/ speed);
+}
+
+function clearScreen(){
+    ctx.fillStyle = 'white';
+    ctx.fillRect(o,o,canvas.clientWidth,canvas.clientWidth,canvas.height);
+
+}
+
+drawGame()
